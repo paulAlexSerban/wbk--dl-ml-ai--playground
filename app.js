3904908a12cd9fc32cc92affdc925108837787c3
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-11 23:51:00 +0200

    pug_03 | added base templates and updtaed routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -17,7 +19,9 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { 
+    pageTitle: '404'
+  });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -6,15 +6,13 @@ const router = express.Router();
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
-/**
- * 1. the use of `path.join()` makes it possible to use Windows and Linux as OS without many issues
- *    - Windows uses paths with back-slash '..\views\shop.html'
- *    - Linux uses paths with forward-slash '..\views\shop.html'/views/shop.html
- */
-
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
-  res.sendFile(path.join(rootDir,'views', 'shop.html')); /* 1 */
+  const products = adminData.products;
+  res.render('shop', {
+    docTitle: "Shop",
+    prods: products 
+  });
 });
 
 module.exports = router;
